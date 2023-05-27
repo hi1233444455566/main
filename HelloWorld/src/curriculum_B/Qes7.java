ecad642a9068ea52f2a55b4edf8e30275a0943fe
michael <michael@localhost>
2023-05-27 09:09:58 +0900

    集約課題1-9〜1-16を再提出します

--- HelloWorld/src/curriculum_B/Qes7.java
+++ HelloWorld/src/curriculum_B/Qes7.java
@@ -5,76 +5,120 @@ import java.util.Scanner;
 public class Qes7 {
 
 	public static void main(String[] args) {
+		
 		// do whileの条件を定義する
 		boolean y = true;
+		
 		// do whileの開始
 		do {
+			
 			// 標準入力
 			Scanner scn = new Scanner(System.in);
+			
 			// メッセージを出力
 			System.out.print("生徒の人数を入力してください（2以上）: ");
+			
 			// 人数の入力
 			int a = scn.nextInt();
+			
 			// 科目の配列を初期化と代入
 			String[] kamoku = { "英語", "数学", "理科", "社会" };
-			// 英語の配列の要素数を取得
+			
+			// 各教科の配列の要素数を取得
 			int[][] tensu = new int[kamoku.length][a];
-			// 生徒の人数だけ点数を繰り返し入力できるようにする
+			
+			// 生徒の人数だけ繰り返し点数の入力ができるようにする
 			for (int b = 0; b < a; b++) {
+				
+				// 科目の数だけ繰り返し点数の入力ができるようにする
+				for(int c = 0; c < kamoku.length; c++) {
+					
 				// メッセージを出力
-				System.out.print(b + 1 + "人目の『英語』の点数を入力してください :");
+				System.out.print(b + 1 + "人目の『" + kamoku[c] + "』の点数を入力してください :");
+				
 				// 点数の入力
-				tensu[0][b] = scn.nextInt();
-				// メッセージを出力
-				System.out.print(b + 1 + "人目の『数学』の点数を入力してください :");
-				// 点数の入力
-				tensu[1][b] = scn.nextInt();
-				// メッセージを出力
-				System.out.print(b + 1 + "人目の『理科』の点数を入力してください :");
-				// 点数の入力
-				tensu[2][b] = scn.nextInt();
-				// メッセージを出力
-				System.out.print(b + 1 + "人目の『社会』の点数を入力してください :");
-				// 点数の入力
-				tensu[3][b] = scn.nextInt();
+				tensu[c][b] = scn.nextInt();
+				
+				}
+				
+				// 一人ずつ最後に改行を出力する
+				System.out.print("\n");
+				
 			}
-			// 各生徒の科目平均点数を算出するための繰り返し
+			
+			// 各生徒の科目平均点数を繰り返し算出する
 			for (int i = 0; i < a; i++) {
-				// 各生徒の合計点数を求める
-				double sumA = tensu[0][i] + tensu[1][i] + tensu[2][i] + tensu[3][i];
+				
+				// 各生徒の合計点数を算出するために使用する
+				double sumA = 0.0d;
+				
+				// 各生徒の平均点数を算出するために使用する
+				double avgA = 0.0d;
+				
+				// 科目の数だけ合計点数と平均点数を繰り返し算出する
+				for(int g = 0; g < kamoku.length; g++) {
+					
+				// 各生徒の合計点数を算出する
+				sumA += tensu[g][i];
+				
 				// 各生徒の平均点数を算出する
-				double avgA = sumA / kamoku.length;
+				avgA = sumA / kamoku.length;
+				
+				}
+				
 				// メッセージを出力
 				System.out.println(i + 1 + String.format("人目の平均点は%.2f", avgA) + "点です。");
+				
 			}
+			
+			// 各生徒の平均点数を出力したら、最後に改行を出力する
+			System.out.print("\n");
+			
 			// 各教科の合計点数を格納する要素数を取得する
 			double[] sumB = new double[kamoku.length];
-			// 各教科の平均点数を求めるための繰り返し（科目数）
+			
+			// 各科目の平均点数を繰り返し算出する
 			for (int k = 0; k < kamoku.length; k++) {
-				// 各教科の平均点数を求めるための繰り返し（人数分）
+				
+				// 生徒の人数だけ平均点数を繰り返し算出する
 				for (int z = 0; z < a; z++) {
-					// 科目ごとの合計点数を求める
+					
+					// 科目ごとの合計点数を算出する
 					sumB[k] += tensu[k][z];
+					
 				}
-				// 科目ごとの平均点数を求める
+				
+				// 科目ごとの平均点数を算出する
 				double avgB = sumB[k] / a;
+				
 				// メッセージを出力する
 				System.out.println(kamoku[k] + String.format("の平均点は%.2f", avgB) + "点です。");
+				
 			}
-			// 全体の合計点数を求めるために使う
+			
+			// 全体の合計点数を算出するために使用する
 			double sumC = 0;
-			// 全体の平均点数を求めるための繰り返し
+			
+			// 全体の平均点数を繰り返し算出する
 			for (int e = 0; e < kamoku.length; e++) {
-				// 全体の合計点数を求める
+				
+				// 全体の合計点数を算出する
 				sumC += sumB[e];
+				
 			}
+			
 			// 全体の平均点数を求める
 			double avgC = sumC / (kamoku.length * a);
+			
 			// メッセージを出力
 			System.out.println(String.format("全体の平均点は%.2f", avgC) + "点です。");
-			// do whileの繰り返しを抜ける
+			
+			// do whileの繰り返しを終了する
 			y = false;
+			
 		// 繰り返しを続ける条件
 		} while (y == true);
+		
 	}
+	
 }
