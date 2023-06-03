8ecaa07e5263bedfa9981ac21e1d771b94c0f02f
michael <michael@localhost>
2023-06-03 21:59:52 +0900

    課題1-19を再提出します

--- HelloWorld/src/curriculum_1_19/Animal.java
+++ HelloWorld/src/curriculum_1_19/Animal.java
@@ -7,28 +7,37 @@ public class Animal {
 
 	public static void main(String[] args) {
 
-		// Dogインスタンスを生成
-		Dog d = new Dog(3);
-
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 
+		// Dogインスタンスを生成
+		Dog f = new Dog();
+
+		// 変数に代入する
+		String name = f.name;
+
 		// コンソールに出力
-		System.out.println(d.name);
+		System.out.println(name);
 
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 
+		// Dogインスタンスを生成
+		Dog d = new Dog(3);
+
+		// 変数に代入する
+		int num = d.num;
+
 		// コンソールに出力
-		System.out.println(d.num);
+		System.out.println(num);
 
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 
 		// 現在日時情報で初期化されたインスタンスの生成
-		Date now = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd H:m:s");
-		
+		Date clock = new Date();
+		SimpleDateFormat timesDay = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+
 		// 日時情報を指定フォーマットの文字列で取得
-		String display = format.format(now);
-		
+		String display = timesDay.format(clock);
+
 		// コンソールに出力
 		System.out.println(display);
 
--- HelloWorld/src/curriculum_1_19/Dog.java
+++ HelloWorld/src/curriculum_1_19/Dog.java
@@ -2,28 +2,31 @@ package curriculum_1_19;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	
+
 	// フィールドを追加
 	String name;
-	
+
 	// Q2：フィールドに動物の数の変数を定義してください。
-	
+
 	// フィールドを追加
 	int num;
-	
+
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	
+
 	// コンストラクタを作成
-	public Dog(int num) {
-		
+	public Dog() {
+
 		// 犬を代入
 		this.name = "犬";
+
+	}
+
+	public Dog(int num) {
 		
 		// 引数を代入
 		this.num = num;
 		
 	}
-	
-	
+
 }
