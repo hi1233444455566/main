634892387750e87e818d1d3491f13ff70d7ef1e3
michael <michael@localhost>
2023-06-05 23:02:34 +0900

    課題1ー22について提出いたします。

---
+++ curiiculum1_22/src/plan/Process.java
@@ -0,0 +1,35 @@
+package plan;
+
+// 処理用クラスを作成
+public class Process {
+
+	// 挨拶フィールドを作成
+	public String hello;
+
+	// 国フィールドを作成
+	public String country;
+
+	// 食べ物フィールドを作成
+	public String food;
+
+	// 料理ジャンルフィールドを作成
+	public String genre;
+
+	// コンストラクタを作成
+	public Process() {
+
+		// 「こんにちは」を代入
+		this.hello = "こんにちは！";
+
+		// 「日本」を代入
+		this.country = "日本";
+
+		// 「寿司」を代入
+		this.food = "寿司";
+
+		// 「和食」を代入
+		this.genre = "和食";
+
+	}
+
+}
