3f987ce3dc51cf7cd8834469426aa59dea5bcd2b
michael <michael@localhost>
2023-06-27 18:02:04 +0900

    課題1ー31について、再提出いたします。よろしくお願いいたします。

--- curiiculum1_31/src/curiiculum1_31/Main.java
+++ curiiculum1_31/src/curiiculum1_31/Main.java
@@ -5,14 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 
 		// インスタンスの生成
-		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
-		Person person2 = new Person("鈴木二郎", 20, 1.7, 60);
+		Person person1 = new Person("鈴木", "太郎", 20, 1.7, 60);
+		Person person2 = new Person("山田", "花子", 22, 1.5, 40);
 		Car car = new Car();
 		Bicycle bicycle = new Bicycle();
 
 		// メソッドに値を渡す（問題4）
-		car.setOwner(person1.name);
-		bicycle.setOwner(person2.name);
+		car.setOwner(person1.fullName());
+		bicycle.setOwner(person2.fullName());
 
 		// コンソールに出力する（問題5）
 		System.out.println(car.getOwner());
--- curiiculum1_31/src/curiiculum1_31/Person.java
+++ curiiculum1_31/src/curiiculum1_31/Person.java
@@ -2,7 +2,8 @@ package curiiculum1_31;
 
 public class Person {
 	// インスタンスフィールドを定義
-	public String name;
+	public String firstName;
+	public String lastName;
 	public int age;
 	public double height;
 	public double weight;
@@ -11,10 +12,11 @@ public class Person {
 	public static int num = 0;
 
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	public Person(String name, int age, double height, double weight) {
+	public Person(String firstname, String lastName, int age, double height, double weight) {
 
 		// 引数をそれぞれ代入
-		this.name = name;
+		this.firstName = firstname;
+		this.lastName = lastName;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
@@ -23,7 +25,15 @@ public class Person {
 		++num;
 	}
 
-	// メソッドを定義
+	// 名前結合メソッドを定義
+	public String fullName() {
+
+		// 結合した名前を返す
+		return this.firstName + this.lastName;
+
+	}
+
+	// 計算メソッドを定義
 	public double bmi() {
 
 		// 計算式を作成
@@ -38,7 +48,7 @@ public class Person {
 	public void print(double bmi) {
 
 		// コンソールに出力する
-		System.out.println("名前は" + this.name + "です");
+		System.out.println("名前は" + this.firstName + "です");
 		System.out.println("年は" + this.age + "です");
 		System.out.println("BMIは" + String.format("%.1f", bmi) + "です");
 		System.out.println("合計" + num + "人です");
@@ -47,20 +57,20 @@ public class Person {
 
 	// 購入メソッドを作成（問題6）
 	public void buy(Car car) {
-		
+
 		// 引数を渡す（問題7）
-		car.setOwner(name);
-		
+		car.setOwner(this.fullName());
+
 		// コンソールに出力する（問題8）
 		System.out.println(car.getOwner() + "が購入しました");
 	}
 
 	// 購入メソッドを作成する（問題9）
 	public void buy(Bicycle bicycle) {
-		
+
 		// 引数を渡す（問題9）
-		bicycle.setOwner(name);
-		
+		bicycle.setOwner(this.fullName());
+
 		// コンソールに出力する（問題9）
 		System.out.println(bicycle.getOwner() + "が購入しました");
 	}
